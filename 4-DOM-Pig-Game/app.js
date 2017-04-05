27fadd3ca2f0a564fcadc4c7151a7d8306a9599b
phchan9 <phckhc@gmail.com>
2017-04-05 12:30:15 +0800

    finished challenge3.2 and 3.3

--- 4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/app.js
@@ -21,10 +21,13 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     }
     
     var dice = Math.floor(Math.random() * 6) + 1;
+    var dice2 = Math.floor(Math.random() * 6) + 1;
     document.querySelector('.dice').style.display = 'block';
+    document.querySelector('.dice2').style.display = 'block';
     document.querySelector('.dice').src = 'dice-' + dice + '.png';
-
-    if (dice === 1) {
+    document.querySelector('.dice2').src = 'dice-' + dice2 + '.png';
+    
+    if (dice === 1 || dice2 === 1) {
         nextPlayer();
     } else {
         if (previousDice === 6 && dice === 6) {
@@ -34,9 +37,9 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
             nextPlayer();
 
         } else {
-            console.log('check');
+            var sumOfTwoDice = dice + dice2;
             previousDice = dice;
-            roundScore += dice;
+            roundScore += sumOfTwoDice; 
             // ui update
             document.querySelector('#current-' + activePlayer).textContent = roundScore;
         }
@@ -51,13 +54,19 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     
     scores[activePlayer] += roundScore;
     document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+
+    var threshold = document.getElementById('input-score').value;
+    if (!threshold) {
+        threshold = 20;
+    }
     
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= threshold) {
         var classNameOfActivePlayerPlanel = '.player-' + activePlayer + '-panel';
         document.querySelector(classNameOfActivePlayerPlanel).classList.toggle('winner');
         document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
         document.getElementById('name-' + activePlayer).textContent = "Winner!";
         document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.dice2').style.display = 'none';
 
         isGamePlaying = false;
     } else {
@@ -66,7 +75,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
 });
 
 document.querySelector('.btn-new').addEventListener('click', init);
-q
+
 function init() {
     scores = [0, 0];
     roundScore = 0;
@@ -90,6 +99,7 @@ function init() {
     document.querySelector('.player-0-panel').classList.add('active');
     
     document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.dice2').style.display = 'none';
 }
 
 function nextPlayer() {
@@ -104,4 +114,5 @@ function nextPlayer() {
     document.querySelector('.player-0-panel').classList.toggle('active');
     document.querySelector('.player-1-panel').classList.toggle('active');
     document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.dice2').style.display = 'none';
 }
