241cb7fa963e922829b60b5a5643c86d27b4d57a
phchan9 <phckhc@gmail.com>
2017-04-11 18:00:02 +0800

    could get input data

---
+++ 6-Budgety/app.js
@@ -0,0 +1,57 @@
+// Budget Controller
+var budgetController = (function() {
+    // some code
+})();
+
+// UI Controller
+var UIController = (function() {
+    var DOMStrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+    
+    
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMStrings.inputType).value,
+                description: document.querySelector(DOMStrings.inputDescription).value,
+                value: document.querySelector(DOMStrings.inputValue).value
+                
+            };
+        },
+        getDOMStrings: function() {
+            return DOMStrings;
+        }
+    }
+    
+})();
+
+// Global App Controller
+var controller = (function(budgetCtrl, UICtrl) {
+
+    var DOM = UICtrl.getDOMStrings();
+    
+    var crtlAddItems = function() {
+        // 1. Get the input
+        var input = UICtrl.getInput();
+        console.log(input);
+        
+        // 2. add the item into budget controller
+        // 3. add  the item to the UI
+        // 4. calculate the budget
+        // 5. display the budget on the UI
+        
+    };
+    
+    document.querySelector(DOM.inputBtn).addEventListener('click', crtlAddItems);
+    document.addEventListener('keypress', function(event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            crtlAddItems();
+        }
+    });
+})(budgetController, UIController);
+
+
