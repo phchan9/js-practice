b614876ce17388cf0f8766bc96a588ad41f46a7b
phchan9 <phckhc@gmail.com>
2017-04-12 15:23:16 +0800

    add data model into budget controller

--- 6-Budgety/app.js
+++ 6-Budgety/app.js
@@ -1,6 +1,29 @@
 // Budget Controller
 var budgetController = (function() {
-    // some code
+    var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var Income = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var data = {
+        allItems: {
+            inc: [],
+            exp: []
+        },
+        totals: {
+            inc: 0,
+            exp: 0
+        }
+    };
+
+    
 })();
 
 // UI Controller
